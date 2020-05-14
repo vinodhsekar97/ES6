3fa0fb0d67a575f81d34be62004160be24c5289d
vinodhsekar97 <64087448+vinodhsekar97@users.noreply.github.com>
2020-05-14 10:06:01 +0530

    Add files via upload

---
+++ headTail.js
@@ -0,0 +1,5 @@
+var b = function () {
+    var _a = [1, 2, 3], head = _a[0], tail = _a.slice(1);
+    console.log(head + " " + tail);
+};
+b();
---
+++ headTail.ts
@@ -0,0 +1,5 @@
+let b = () =>{
+let[head, ...tail] = [1, 2, 3];
+console.log( `${head} ${tail}`);
+}
+b();
